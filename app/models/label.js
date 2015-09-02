c6596a2ebda1864abec424f828fa376a67592964
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-09-02 05:54:45 -0300

    label model

---
+++ app/models/label.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var LabelSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    project: {
+        type: ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+LabelSchema.post('validate', function(doc) {
+  doc.updated_at = Date.now();
+});
+
+module.exports = mongoose.model('User', LabelSchema);
