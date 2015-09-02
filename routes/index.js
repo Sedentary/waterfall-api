2d70a42aae5ae41c3ded0cb6f16c0ae6e3e24299
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-09-02 05:17:19 -0300

    init project

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  return res.status(200).json({ message: 'ok' });
+});
+
+module.exports = router;
