beeebe6b886acc64fb6dee192fc7e0814caa5c99
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-09-02 05:33:11 -0300

    added user model

---
+++ app/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.post('validate', function(doc) {
+  doc.updated_at = Date.now();
+});
+
+module.exports = mongoose.model('User', UserSchema);
