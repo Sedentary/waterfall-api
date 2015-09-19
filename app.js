b9aecdbd4b245e8d941d16af014ba44f8b88559d
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2015-09-19 01:58:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Database connection handler
 require('./config/mongoose');
@@ -11,6 +12,7 @@ require('./config/mongoose');
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
